3fec1fc1be39df49507b94b2b52d368e592eaf96
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-07-04 17:20:18 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 nodejs_version=16
 
-YNH_PHP_VERSION="8.0"
+YNH_PHP_VERSION="8.1"
 
 pkg_dependencies="sqlite3 php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd"
 
