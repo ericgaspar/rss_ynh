42df78d749de1bfbfdeccc04c48bdcfe33ce3819
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-07-04 17:02:40 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+
+YNH_PHP_VERSION="8.0"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd"
+
+YNH_COMPOSER_VERSION=2.3.5
 
 #=================================================
 # PERSONAL HELPERS
