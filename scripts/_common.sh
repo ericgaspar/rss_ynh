a8701639c23cc6fc5c20732b737d95318532e2ce
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-07-04 19:40:52 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,9 +8,9 @@ nodejs_version=16
 
 YNH_PHP_VERSION="8.1"
 
-pkg_dependencies="sqlite3 php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd"
+pkg_dependencies="sqlite3 php${YNH_PHP_VERSION}-sqlite3"
 
-YNH_COMPOSER_VERSION=2.3.5
+YNH_COMPOSER_VERSION=2.3.7
 
 #=================================================
 # PERSONAL HELPERS
